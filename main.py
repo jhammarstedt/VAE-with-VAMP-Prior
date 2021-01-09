fb8f2dd0329b6e55c4fc05ab9cc4de2b41d865af
Matej Sestak <sestamat@fel.cvut.cz>
2021-01-09 16:15:04 +0100

    added validation loop and img plotting

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from model import VanillaVAE
 from load_data import load_dynamic_mnist
 from torch.optim import Adam
-from utils import train_vae
+from utils import train_loop, val_loop
 from torch.utils.tensorboard import SummaryWriter
 import matplotlib.pyplot as plt
 
@@ -17,11 +17,6 @@ args = {
 
 train_loader, val_loader, test_loader, input_size = load_dynamic_mnist(args['batch_size'])
 
-# data, target = next(iter(train_loader))
-#
-# plt.imshow(data[0].reshape(28, 28))
-# plt.show()
-
 writer = SummaryWriter()
 
 train_loss_history = []
@@ -32,10 +27,11 @@ model = VanillaVAE(input_size=input_size[1], hidden_dims=100, latent_dims=10, ar
 optimizer = Adam(model.parameters(), lr=args['lr'])
 
 for epoch in range(1, args['epochs'] + 1):
-    model, tr_loss_e, tr_re_e, tr_kl_e = train_vae(train_loader=train_loader, model=model, optimizer=optimizer, writer=writer, epoch=epoch)
+    model, tr_loss_e, tr_re_e, tr_kl_e = train_loop(train_loader=train_loader, model=model, optimizer=optimizer, writer=writer, epoch=epoch)
+    val_loss, val_re, val_kl = val_loop(val_loader, model, writer, epoch, plot=True)
     train_loss_history.append(tr_loss_e)
     train_re_history.append(tr_re_e)
     train_kl_history.append(tr_kl_e)
-    print("Epoch {},\t loss: {},\t reconstruction loss: {:.3f},\t KL: {:.3f}".format(epoch, tr_loss_e, tr_re_e, tr_kl_e))
+    print("Epoch {},\t train_loss: {:.2f}\t(RL = {:.2f},\tKL: {:.2f})\t val_loss {:.2f}\t(RL = {:.2f},\tKL: {:.2f})".format(epoch, tr_loss_e, tr_re_e, tr_kl_e, val_loss, val_re, val_kl))
 
 writer.close()
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import torch
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
 
-
-def train_vae(train_loader, model, optimizer, writer, epoch):
+def train_loop(train_loader, model, optimizer, writer, epoch):
     # set loss to 0
     train_loss = 0
     train_re = 0
@@ -42,6 +43,43 @@ def train_vae(train_loader, model, optimizer, writer, epoch):
     return model, train_loss, train_re, train_kl
 
 
+def val_loop(val_loader, model, writer, epoch, plot=False, dir=''):
+    # set loss to 0
+    val_loss = 0
+    val_re = 0
+    val_kl = 0
+    # set model in training mode
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.eval()
+
+    for id, (x, _) in enumerate(val_loader):
+        x = x.to(device)
+        # loss evaluation (forward pass)
+        loss, RE, KL = model.get_loss(x)
+
+        writer.add_scalar("Loss", loss, epoch)
+        writer.add_scalar("RE", RE, epoch)
+        writer.add_scalar("KL", KL, epoch)
+
+        val_loss += loss.item()
+        val_re += RE.item()
+        val_kl += KL.item()
+        if plot and id == 1:
+            if epoch == 1:
+                if not os.path.exists(dir + 'reconstruction/'):
+                    os.makedirs(dir + 'reconstruction/')
+                plot_images(x.cpu().detach().numpy()[:9], dir + 'reconstruction/', 'real')
+            reconstruction, _, _, _, _ = model.forward(x)
+            plot_images(reconstruction.cpu().detach().numpy()[:9], dir + 'reconstruction/', str(epoch))
+    writer.flush()
+
+    # calculate final loss
+    val_loss /= len(val_loader)  # loss function already averages over batch size
+    val_re /= len(val_loader)  # re already averages over batch size
+    val_kl /= len(val_loader)  # kl already averages over batch size
+    return val_loss, val_re, val_kl
+
 def log_Normal_diag(x, mean, log_var, average=False, dim=None):
     log_normal = -0.5 * (log_var + torch.pow(x - mean, 2) / torch.exp(log_var))
     if average:
@@ -70,3 +108,21 @@ def log_Normal_standard(x, average=False, dim=None):
 #     plt.grid(True)
 #     # plt.savefig(f"{}")
 #     plt.show()
+
+def plot_images(x_sample, dir, name, size_x=3, size_y=3):
+
+    fig = plt.figure(figsize=(size_x, size_y))
+    gs = gridspec.GridSpec(size_x, size_y)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, sample in enumerate(x_sample):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        sample = sample.reshape((28, 28))
+        plt.imshow(sample)
+
+    plt.savefig(dir + name + '.png', bbox_inches='tight')
+    plt.close(fig)
