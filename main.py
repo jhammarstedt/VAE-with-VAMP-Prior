52f5bfbb75b99b4ea3b56a561003229efa8d2597
Matej Sestak <sestamat@fel.cvut.cz>
2021-01-11 15:17:16 +0100

    run experiment method

--- main.py
+++ main.py
@@ -24,50 +24,50 @@ args = {
 
 print(args)
 
-# load data
-train_loader, val_loader, test_loader, input_size = load_dataset(args)
 
+def run_experiment(args):
+    # load data
+    train_loader, val_loader, test_loader, input_size = load_dataset(args)
 
+    run_name = '{}_{}_{}_{}'.format(args['dataset'], args['prior'], args['psudo_inp'], args['latent_dims'])
+    model_name = run_name + '.model'
+    model = VAE_model(input_size=input_size[1], args=args)
 
-run_name = '{}_{}_{}_{}'.format(args['dataset'], args['prior'], args['psudo_inp'], args['latent_dims'])
-model_name = run_name + '.model'
-model = VAE_model(input_size=input_size[1], args=args)
+    if args['train']:
+        optimizer = Adam(model.parameters(), lr=args['lr'])
+        writer = SummaryWriter()
 
-if args['train']:
-    optimizer = Adam(model.parameters(), lr=args['lr'])
-    writer = SummaryWriter()
+        train_loss_history = []
+        train_re_history = []
+        train_kl_history = []
 
-    train_loss_history = []
-    train_re_history = []
-    train_kl_history = []
+        val_loss_history = []
+        val_re_history = []
+        val_kl_history = []
 
-    val_loss_history = []
-    val_re_history = []
-    val_kl_history = []
+        best_val_loss = 1e10
+        for epoch in range(1, args['epochs'] + 1):
+            start = time.time()
+            model, tr_loss_e, tr_re_e, tr_kl_e = train_loop(train_loader=train_loader, model=model, optimizer=optimizer, writer=writer, epoch=epoch)
+            val_loss_e, val_re_e, val_kl_e = val_loop(val_loader, model, writer, epoch, plot=True)
 
-    best_val_loss = 1e10
-    for epoch in range(1, args['epochs'] + 1):
-        start = time.time()
-        model, tr_loss_e, tr_re_e, tr_kl_e = train_loop(train_loader=train_loader, model=model, optimizer=optimizer, writer=writer, epoch=epoch)
-        val_loss_e, val_re_e, val_kl_e = val_loop(val_loader, model, writer, epoch, plot=True)
+            train_loss_history.append(tr_loss_e)
+            train_re_history.append(tr_re_e)
+            train_kl_history.append(tr_kl_e)
 
-        train_loss_history.append(tr_loss_e)
-        train_re_history.append(tr_re_e)
-        train_kl_history.append(tr_kl_e)
+            val_loss_history.append(val_loss_e)
+            val_re_history.append(val_re_e)
+            val_kl_history.append(val_kl_e)
+            end = time.time()
+            print("Epoch {}\ttime {:.2f}s,\t train_loss: {:.2f}\t(RL = {:.2f},\tKL: {:.2f})\t val_loss {:.2f}\t(RL = {:.2f},\tKL: {:.2f})".format(epoch, end - start, tr_loss_e, tr_re_e, tr_kl_e, val_loss_e, val_re_e, val_kl_e))
+            if val_loss_e < best_val_loss:
+                best_val_loss = val_loss_e
+                print('Saving model with {} validation loss'.format(best_val_loss))
+                torch.save(model.state_dict(), model_name)
 
-        val_loss_history.append(val_loss_e)
-        val_re_history.append(val_re_e)
-        val_kl_history.append(val_kl_e)
-        end = time.time()
-        print("Epoch {}\ttime {:.2f}s,\t train_loss: {:.2f}\t(RL = {:.2f},\tKL: {:.2f})\t val_loss {:.2f}\t(RL = {:.2f},\tKL: {:.2f})".format(epoch, end - start, tr_loss_e, tr_re_e, tr_kl_e, val_loss_e, val_re_e, val_kl_e))
-        if val_loss_e < best_val_loss:
-            best_val_loss = val_loss_e
-            print('Saving model with {} validation loss'.format(best_val_loss))
-            torch.save(model.state_dict(), model_name)
+        writer.close()
 
-    writer.close()
-
-if args['test']:
-    model.load_state_dict(torch.load(model_name))
-    test_loop(test_loader, model)
+    if args['test']:
+        model.load_state_dict(torch.load(model_name))
+        test_loop(test_loader, model)
 
